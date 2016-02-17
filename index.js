a84989a6970f62e07ac2e24304a1051590979084
straker <stevenlambert503@gmail.com>
2016-02-16 18:19:01 -0700

    remove let's as Firefox 42 doesn't like them

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 "use strict";
 
 if (typeof window.html === 'undefined') {
-  let tagNameRegex = /<([\w:-]+)/;
-  let attributesRegex = /<[\w:-]+\s+([^/>]+)\/?>/;
-  let whiteSpaceRegex = /\s+/;
-  let attributeValueRegex = /^(?:'|")?(.+?)(?:'|")?$/;
-  let childElements = /<([\w:-]+)[^>]*>([\s\S]*)(?:<\/\1>)/;
+  var tagNameRegex = /<([\w:-]+)/;
+  var attributesRegex = /<[\w:-]+\s+([^/>]+)\/?>/;
+  var whiteSpaceRegex = /\s+/;
+  var attributeValueRegex = /^(?:'|")?(.+?)(?:'|")?$/;
+  var childElements = /<([\w:-]+)[^>]*>([\s\S]*)(?:<\/\1>)/;
 
   window.html = function(strings, ...values) {
     // break early if called with empty content
@@ -17,8 +17,8 @@ if (typeof window.html === 'undefined') {
     // insert placeholders into the generated string so we can run it through the
     // HTML parser without any malicious content.
     // (this particular placeholder will even work when used to create a DOM element)
-    let str = strings[0];
-    for (let i = 0; i < values.length; i++) {
+    var str = strings[0];
+    for (var i = 0; i < values.length; i++) {
       str += 'substitutionIndex:' + i + '' + strings[i+1];
     }
 
@@ -31,20 +31,20 @@ if (typeof window.html === 'undefined') {
     // create the first tag with all it's attributes and then innerHTML it's children.
     // this avoids problems with having the HTML parser return nothing when passed
     // elements outside of a context (such as <tr>)
-    let tagName = tagNameRegex.exec(str)[1].toLowerCase();
-    let frag = document.createDocumentFragment();
-    let root = document.createElement(tagName);
+    var tagName = tagNameRegex.exec(str)[1].toLowerCase();
+    var frag = document.createDocumentFragment();
+    var root = document.createElement(tagName);
 
     frag.appendChild(root);
 
     // attributes
     if (attributesRegex.test(str)) {
-      let attributes = attributesRegex.exec(str)[1].trim().split(whiteSpaceRegex);
+      var attributes = attributesRegex.exec(str)[1].trim().split(whiteSpaceRegex);
 
-      for (let j = 0; j < attributes.length; j++) {
-        let attr = attributes[j].split('=');
-        let name = attr[0];
-        let value = (attr[1] ? attributeValueRegex.exec(attr[1])[1] : '');
+      for (var j = 0; j < attributes.length; j++) {
+        var attr = attributes[j].split('=');
+        var name = attr[0];
+        var value = (attr[1] ? attributeValueRegex.exec(attr[1])[1] : '');
 
         root.setAttribute(name, value);
       }
